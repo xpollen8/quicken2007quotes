d07ddb5765e4095f598937380536f979e04f4668
xpollen8 <github@xpollen8.com>
2024-04-16 14:02:36 -0700

    dw: break fetching of quotes into own module

---
+++ fetchQuotes.cjs
@@ -0,0 +1,38 @@
+const fetchQuotes = async (credentials, symbols) => {
+	const fetch = require('node-fetch');
+	const date = new Date();
+	const today = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+
+	const fetcBatch = async (symbols) => {
+		const url = `https://yh-finance.p.rapidapi.com/market/v2/get-quotes?region=US&symbols=${symbols}`;
+		const options = {
+			method: 'GET',
+			headers: credentials,
+		};
+
+		const response = await fetch(url, options);
+		const resp = await response.json();
+		const res = resp?.quoteResponse?.result?.map(r => {
+			const { symbol, regularMarketPrice, averageDailyVolume10Day } = r;
+			return [ symbol, regularMarketPrice, 0, 0, JSON.stringify(averageDailyVolume10Day) || '' ];
+		});
+		return res;
+	}
+
+	if (credentials['X-RapidAPI-Key'] === 'YOUR-KEY-HERE-SEE-README') {
+		throw "You must edit the rapidapi.json file - See README.md";
+	}
+	// cannot fetch all in one batch, so break 'em up
+	const symbols1 = symbols.slice(0, 30).join('%2C');
+	const symbols2 = symbols.slice(30).join('%2C');
+	const res1 = (symbols1?.length) ? await fetcBatch(symbols1) : [];
+	const res2 = (symbols2?.length) ? await fetcBatch(symbols2) : [];
+	return [ ...res1, ...res2 ].map(([ Symbol, Last, High, Low, Volume ]) => {
+		const symbol = Symbol.replace('-', '.');	// BRK-B -> BRK.B
+		return {
+			Symbol: symbol, Last, Today: today, High, Low, Volume: Volume.replace(/,/g, '')
+		}
+	});
+}
+
+module.exports = fetchQuotes;
--- quotes.cjs
+++ quotes.cjs
@@ -1,41 +1,14 @@
-const fetch = require('node-fetch');
-
-const date = new Date();
-const today = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+const fetchQuotes = require('./fetchQuotes.cjs');
 const symbolsArray = require('./symbols.json');
-const RapidAPI = require('./rapidapi.json');
+const credentials = require('./rapidapi.json');
 
 (async () => {
 	try {
-		const fetcBatch = async (symbols) => {
-			const url = `https://yh-finance.p.rapidapi.com/market/v2/get-quotes?region=US&symbols=${symbols}`;
-			const options = {
-				method: 'GET',
-				headers: RapidAPI,
-			};
-
-			const response = await fetch(url, options);
-			const resp = await response.json();
-			const res = resp?.quoteResponse?.result?.map(r => {
-				const { symbol, regularMarketPrice, averageDailyVolume10Day } = r;
-				return [ symbol, regularMarketPrice, 0, 0, JSON.stringify(averageDailyVolume10Day) || '' ];
-			});
-			return res;
-		}
-
-		if (RapidAPI['X-RapidAPI-Key'] === 'YOUR-KEY-HERE-SEE-README') {
-			throw "You must edit the rapidapi.json file - See README.md";
-		}
-		// cannot fetch all in one batch, so break 'em up
-		const symbols1 = symbolsArray.slice(0, 30).join('%2C');
-		const symbols2 = symbolsArray.slice(30).join('%2C');
-		const res1 = (symbols1?.length) ? await fetcBatch(symbols1) : [];
-		const res2 = (symbols2?.length) ? await fetcBatch(symbols2) : [];
 		console.log(`!Type:Prices`);
 		console.log(`Type:Prices`);
-		[ ...res1, ...res2 ].forEach(([ Symbol, Last, High, Low, Volume ]) => {
-			const symbol = Symbol.replace('BRK-B', 'BRK.B');
-			console.log(`${symbol},${Last},${today},${High},${Low},${Volume.replace(/,/g, '')}`);
+		const results = await fetchQuotes(credentials, symbolsArray);
+		results.forEach(({ Symbol, Last, Today, High, Low, Volume }) => {
+			console.log(`${Symbol},${Last},${Today},${High},${Low},${Volume}`);
 		});
 		console.log(`^`);
 	} catch (error) {
